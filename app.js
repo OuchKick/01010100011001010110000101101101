a425289ee4907457d6d4c0d8113e2031982530da
Adam Reed <thirdeyeadam@gmail.com>
2021-04-18 14:35:00 -0500

    Code works, outputs HTML in the Output Folder - need to do README

--- app.js
+++ app.js
@@ -1,111 +1,186 @@
-const Manager = require("./lib/Manager");
+
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 const inquirer = require("inquirer");
-const path = require("path");
 const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-const render = require("./lib/htmlRenderer");
-
-const teamMembers = [];
 
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-function addTeamMember(){
-    return inquirer.prompt([
-        {
-            type: 'list',
-            name: 'role',
-            message: 'Select your current role.',
-            choices: ['Manager', 'Engineer', 'Intern']
-        },
-        {
-            type: 'input',
-            name: 'name',
-            message: 'We know your colleagues call you a name. We are not able to spell it. What is your name?',
-            
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: 'We gave you an ID after you were brought here. Provide it now.',
-            
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: 'We have found in your memory something called email. Seems useful. Maintain contact outside of permitted work time with permitted Si only. Provide email now.',
-            
-        },
-        {
-            type: 'input',
-            name: 'officenumber',
-            message: 'If you check your left pocket, you will find an object with a reactive image processing unit. This is ONLY for contact with permitted SiLi. Provide a 10 digit long identifier for contact.',
-            // validate: function (userInput) {
-            //     return (userInput.length < 1) ? console.log('Stop fooling around.') : 
-            //     (userInput.length > 10) ? console.log('Stop fooling around.'): true;
-                
-            // } 
-            
-        },
-        {
-            type: 'list',
-            name: 'role',
-            message: 'Add additional Si if needed.',
-            choices: ['Engineer', 'Intern', 'None']
-        },
+const employees = [];
 
+// Runs HTML creation and member function
 
-
-
-
-
-
-
-
-
-
-
-    ])
+function init() {
+    startHtml();
+    addMember();
 }
 
 
+// Questions
+function addMember() {
+    inquirer.prompt([{
+        message: "Enter team member's name",
+        name: "name"
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "Select team member's role",
+        choices: [ "Engineer","Intern", "Manager"]
+        
+    },
+    {
+        message: "Enter team member's id",
+        name: "id"
+    },
+    {
+        message: "Enter team member's email address",
+        name: "email"
+    }])
+    // Based on role selection, decides next question after standard questions from Employee Parent
+    .then(function({name, role, id, email}) {
+        let roleInfo = "";
+        if (role === "Engineer") {
+            roleInfo = "GitHub username";
+        } else if (role === "Intern") {
+            roleInfo = "school name";
+        } else {
+            roleInfo = "office phone number";
+        }
+        inquirer.prompt([{
+            message: `Enter team member's ${roleInfo}`,
+            name: "roleInfo"
+        },
+        {
+            type: "list",
+            message: "Would you like to add more team members?",
+            choices: ["Yes", "No"],
+            name: "additionalMembers"
+        }])
+        .then(function({roleInfo, additionalMembers}) {
+            let newMember;
+            if (role === "Engineer") {
+                newMember = new Engineer(name, id, email, roleInfo);
+            } else if (role === "Intern") {
+                newMember = new Intern(name, id, email, roleInfo);
+            } else {
+                newMember = new Manager(name, id, email, roleInfo);
+            }
+            employees.push(newMember);
+            addHtml(newMember)
+            .then(function() {
+                if (additionalMembers === "Yes") {
+                    addMember();
+                } else {
+                    htmlDone();
+                }
+            });
+            
+        });
+    });
+}
+
+// Starter HTML for the test
+function startHtml() {
+    const html = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <title>Team Profile</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-dark bg-danger mb-5">
+            <span class="navbar-brand mb-0 h1 w-100 text-center">Team Profile</span>
+        </nav>
+        <div class="container">
+            <div class="row">`;
+            // Writes to index.html in the output folder
+    fs.writeFile("./output/index.html", html, function(err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+    console.log("Starting!");
+}
+// Additional HTML for cards, dedicated for each team member
+function addHtml(member) {
+    return new Promise(function(resolve, reject) {
+        const name = member.getName();
+        const role = member.getRole();
+        const id = member.getId();
+        const email = member.getEmail();
+        let data = "";
+        if (role === "Engineer") {
+            const gitHub = member.getGithub();
+            data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Engineer</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: ${email}</li>
+                <li class="list-group-item">GitHub: ${gitHub}</li>
+            </ul>
+            </div>
+        </div>`;
+        } else if (role === "Intern") {
+            const school = member.getSchool();
+            data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Intern</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: ${email}</li>
+                <li class="list-group-item">School: ${school}</li>
+            </ul>
+            </div>
+        </div>`;
+        } else {
+            const officePhone = member.getOfficeNumber();
+            data = `<div class="col-6">
+            <div class="card mx-auto mb-3" style="width: 18rem">
+            <h5 class="card-header">${name}<br /><br />Manager</h5>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">ID: ${id}</li>
+                <li class="list-group-item">Email Address: ${email}</li>
+                <li class="list-group-item">Office Phone: ${officePhone}</li>
+            </ul>
+            </div>
+        </div>`
+        }
+        console.log("Adding Member!");
+        fs.appendFile("./output/index.html", data, function (err) {
+            if (err) {
+                return reject(err);
+            };
+            return resolve();
+        });
+    });
+    
+            
+    
+        
+    
+    
+}
+
+function htmlDone() {
+    const html = ` </div>
+    </div>
+    
+</body>
+</html>`;
+
+    fs.appendFile("./output/index.html", html, function (err) {
+        if (err) {
+            console.log(err);
+        };
+    });
+    console.log("Completed!");
+}
 
 
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-
-
-
-
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-
-
-
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-
-
-
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+init();
